e085070b28a333e7d074b6efeea1d8e3eae60517
YRK <yrk199512@gmail.com>
2021-06-23 09:54:02 +0800

    1. 完善报警相关界面

---
+++ src/components/EmptyPlaceholder/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div v-show="isShow" class="main-div">
+    <label>暂无内容</label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EmptyPlaceholder',
+  props: {
+    isShow: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+.main-div {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+}
+</style>
